56b0c9f6c9a47073b797510be9864779db565fa7
salamat9 <salamatbaichoroev@gmail.com>
2023-01-05 23:13:10 +0600

    head component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Learn next</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />;
+		</>
+	);
 }
